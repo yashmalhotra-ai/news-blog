55d06133b5ddad93182f462f5636bbe34d57eabf
yashmalhotra-ai <captinyashiphone@gmail.com>
2024-08-03 02:53:45 +0530

    Added advertisement card,fixed bugs UI

--- src/Components/RecentlyNews.js
+++ src/Components/RecentlyNews.js
@@ -1,20 +1,37 @@
-import React from 'react'
-import RecentlyNewsCard from '../Models/RecentlyNewsCard'
+import React from 'react';
+import RecentlyNewsCard from '../Models/RecentlyNewsCard';
+import TopAuthorsCard from '../Models/TopAuthorsCard';
+import AdvertisementCard from '../Models/AdvertisementCard';
 
 const RecentlyNews = () => {
     return (
-        <section className="text-gray-600 body-font overflow-hidden bg-[#F2F8F7]">
+        <section className="text-gray-600 body-font overflow-hidden">
             <div className="px-5 py-12 mx-auto">
-                <h3 className='font-semibold font-sans text-xl'><span className='bg-[#00AAA1] text-white'>Recently</span>Posted</h3>
-                <div className="lg:w-full mx-auto flex flex-col lg:flex-row ">
-                <RecentlyNewsCard/>
-                    <div className=" lg:w-1/3 w-full m-1">
-                        <h3 className='font-semibold font-sans text-xl'><span className='bg-[#00AAA1] text-white'>Populer</span> Posted</h3>
+                <h3 className='font-semibold font-sans text-xl'>
+                    <span className='bg-[#00AAA1] text-white'>Recently</span> Posted
+                </h3>
+                <div className="lg:w-full mx-auto flex flex-col lg:flex-row gap-4">
+                    <div className="flex-1 lg:pr-5">
+                        <RecentlyNewsCard />
+                    </div>
+                    <div className="lg:w-1/3 w-full m-1">
+                        <h3 className='font-semibold font-sans text-xl'>
+                            <span className='bg-[#00AAA1] text-white'>Top</span> Authors
+                        </h3>
+                        <div>
+                            <TopAuthorsCard />
+                            <TopAuthorsCard />
+                            <TopAuthorsCard />
+                        </div>
+                        <div className='mt-10'>
+                            <AdvertisementCard />
+                        </div>
+                        {/* Add content for Popular Posted section here */}
                     </div>
                 </div>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default RecentlyNews
+export default RecentlyNews;
--- src/Models/RecentlyNewsCard.js
+++ src/Models/RecentlyNewsCard.js
@@ -1,52 +1,65 @@
-import React from 'react'
+import React from 'react';
 
 const RecentlyNewsCard = () => {
     const data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    console.log(data)
     const newsCard = data.map((item, index) => {
+        // console.log(item);
         return (
-            <div key={index} className='flex gap-2 mt-4'>
-                <div>
-                    <img className='rounded-lg' src="https://dummyimage.com/250x180" alt="" />
-
+            <React.Fragment key={index}>
+                <div className='flex flex-col md:flex-row gap-4 mt-4'>
+                    <div className='flex-shrink-0'>
+                        <img className='rounded-lg w-full h-auto max-w-xs md:max-w-sm' src="https://dummyimage.com/250x180" alt="" />
+                    </div>
+                    <div className='flex-1'>
+                        <button className='bg-[#DFF1F0] p-[1px] rounded font-semibold text-[12px] text-[#666666]'>
+                            Travel
+                        </button>
+                        <h5 className="text-gray-900 text-xl title-font font-semibold mb-1">Design is the Mix of emotions</h5>
+                        <div className='w-max flex flex-wrap items-center mt-2 gap-5 text-[#777777] text-xs'>
+                            <div className='flex items-center justify-center gap-2 border-r border-[#999999] pr-2'>
+                                <img className='rounded-full' src="https://dummyimage.com/18x18" alt="" />
+                                <p>Author</p>
+                            </div>
+                            <div className='flex items-center justify-center gap-2 border-r border-[#999999] pr-2'>
+                                <svg width="11" height="12" viewBox="0 0 11 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M2.4 5.4H3.6V6.6H2.4V5.4ZM10.8 2.4V10.8C10.8 11.46 10.26 12 9.6 12H1.2C0.88174 12 0.576515 11.8736 0.351472 11.6485C0.126428 11.4235 0 11.1183 0 10.8L0.00599999 2.4C0.00599999 1.74 0.534 1.2 1.2 1.2H1.8V0H3V1.2H7.8V0H9V1.2H9.6C10.26 1.2 10.8 1.74 10.8 2.4ZM1.2 3.6H9.6V2.4H1.2V3.6ZM9.6 10.8V4.8H1.2V10.8H9.6ZM7.2 6.6H8.4V5.4H7.2V6.6ZM4.8 6.6H6V5.4H4.8V6.6Z" fill="#555555" />
+                                </svg>
+                                <p>Date</p>
+                            </div>
+                            <div className='flex items-center justify-center gap-2 pr-2'>
+                                <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M6 0C2.6916 0 0 2.6916 0 6C0 9.3084 2.6916 12 6 12C9.3084 12 12 9.3084 12 6C12 2.6916 9.3084 0 6 0ZM6 10.8C3.3534 10.8 1.2 8.6466 1.2 6C1.2 3.3534 3.3534 1.2 6 1.2C8.6466 1.2 10.8 3.3534 10.8 6C10.8 8.6466 8.6466 10.8 6 10.8Z" fill="#555555" />
+                                    <path d="M6.60009 3H5.40009V6.2484L7.37589 8.2242L8.22429 7.3758L6.60009 5.7516V3Z" fill="#555555" />
+                                </svg>
+                                3 Min. To Read
+                            </div>
+                        </div>
+                        <p className="text-sm text-[#555555] font-medium mt-4">Did you come here for something in particular or just general Riker-bashing? And blowing into maximum warp</p>
+                    </div>
                 </div>
-                <div>
+                {index === 3 && (
+                    <div className='flex-col justify-center items-center text-center bg-[#F2F8F7] mt-4 p-6 text-black rounded-md'>
+                        <p className='font-sans font-semibold text-xl'>Share Your Knowledge With Our Readers</p>
+                        <div className='flex items-center justify-center mt-4 mb-4'>
+                            <button className='flex items-center justify-center gap-2 bg-[#DFF1F0] p-2 rounded font-semibold text-[#666666] mt-2 border border-[#00AAA1] '>
+                                <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M13.357 0.351803L13.7436 0.738392C14.2763 1.27175 14.1957 2.21726 13.5621 2.85022L5.58982 10.8225L3.00688 11.7673C2.68253 11.8866 2.36671 11.7319 2.3025 11.4233C2.2808 11.3114 2.29101 11.1956 2.33198 11.0892L3.29518 8.48393L11.2452 0.533303C11.8788 -0.0996548 12.8243 -0.180904 13.357 0.352458V0.351803ZM5.24189 1.14464C5.32793 1.14464 5.41314 1.16159 5.49263 1.19451C5.57213 1.22744 5.64436 1.27571 5.70521 1.33655C5.76605 1.3974 5.81432 1.46963 5.84724 1.54913C5.88017 1.62862 5.89712 1.71383 5.89712 1.79987C5.89712 1.88592 5.88017 1.97112 5.84724 2.05062C5.81432 2.13012 5.76605 2.20235 5.70521 2.2632C5.64436 2.32404 5.57213 2.3723 5.49263 2.40523C5.41314 2.43816 5.32793 2.45511 5.24189 2.45511H2.62094C2.27338 2.45511 1.94006 2.59318 1.6943 2.83894C1.44854 3.0847 1.31047 3.41802 1.31047 3.76558V11.6284C1.31047 11.976 1.44854 12.3093 1.6943 12.5551C1.94006 12.8008 2.27338 12.9389 2.62094 12.9389H10.4838C10.8313 12.9389 11.1647 12.8008 11.4104 12.5551C11.6562 12.3093 11.7942 11.976 11.7942 11.6284V9.00747C11.7942 8.83369 11.8633 8.66703 11.9862 8.54414C12.109 8.42126 12.2757 8.35223 12.4495 8.35223C12.6233 8.35223 12.7899 8.42126 12.9128 8.54414C13.0357 8.66703 13.1047 8.83369 13.1047 9.00747V11.6284C13.1047 12.3235 12.8286 12.9902 12.3371 13.4817C11.8455 13.9732 11.1789 14.2494 10.4838 14.2494H2.62094C1.92583 14.2494 1.25918 13.9732 0.767656 13.4817C0.276134 12.9902 0 12.3235 0 11.6284V3.76558C0 3.07046 0.276134 2.40382 0.767656 1.91229C1.25918 1.42077 1.92583 1.14464 2.62094 1.14464H5.24189Z" fill="#00AAA1" />
+                                </svg>
 
-                    <button className='bg-[#DFF1F0] p-[1px] rounded font-semibold text-[12px] text-[#666666]'>
-                        Travel
-                    </button>
-
-                    <h5 className="text-gray-900 text-xl title-font font-semibold mb-1">Design is the Mix of emotions</h5>
-                    <div className='w-max flex items-center mt-2 gap-5  text-[#777777] text-xs '>
-                        <div className='flex items-center justify-center gap-2 border-r border-[#999999] pr-2'>
-                            <img className='rounded-full' src="https://dummyimage.com/18x18" alt="" />
-                            <p>Author</p>
-                        </div>
-                        <div className='flex items-center justify-center gap-2 border-r border-[#999999] pr-2'><svg width="11" height="12" viewBox="0 0 11 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M2.4 5.4H3.6V6.6H2.4V5.4ZM10.8 2.4V10.8C10.8 11.46 10.26 12 9.6 12H1.2C0.88174 12 0.576515 11.8736 0.351472 11.6485C0.126428 11.4235 0 11.1183 0 10.8L0.00599999 2.4C0.00599999 1.74 0.534 1.2 1.2 1.2H1.8V0H3V1.2H7.8V0H9V1.2H9.6C10.26 1.2 10.8 1.74 10.8 2.4ZM1.2 3.6H9.6V2.4H1.2V3.6ZM9.6 10.8V4.8H1.2V10.8H9.6ZM7.2 6.6H8.4V5.4H7.2V6.6ZM4.8 6.6H6V5.4H4.8V6.6Z" fill="#555555" />
-                        </svg>
-
-                            <p>Date</p>
-                        </div>
-                        <div className='flex items-center justify-center gap-2  pr-2'>
-                            <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M6 0C2.6916 0 0 2.6916 0 6C0 9.3084 2.6916 12 6 12C9.3084 12 12 9.3084 12 6C12 2.6916 9.3084 0 6 0ZM6 10.8C3.3534 10.8 1.2 8.6466 1.2 6C1.2 3.3534 3.3534 1.2 6 1.2C8.6466 1.2 10.8 3.3534 10.8 6C10.8 8.6466 8.6466 10.8 6 10.8Z" fill="#555555" />
-                                <path d="M6.60009 3H5.40009V6.2484L7.37589 8.2242L8.22429 7.3758L6.60009 5.7516V3Z" fill="#555555" />
-                            </svg>
-                            3 Min. To Read
+                                <span>Write-on Notebook</span>
+                            </button>
                         </div>
                     </div>
-                <p className=" text-sm  text-[#555555] font-medium mt-4">Did you come here for something in fparticular or just general Riker-bashing? And blowing into maximum warp</p>
-                </div>
-            </div>
-        )
-    })
+                )}
+            </React.Fragment>
+        );
+    });
+
     return (
-        <div className='pl-2 mt-10  border border-black' >
+        <div className='pl-2 mt-10'>
             {newsCard}
-
         </div>
-    )
-}
+    );
+};
 
-export default RecentlyNewsCard
+export default RecentlyNewsCard;
